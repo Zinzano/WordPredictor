c01ceb6fa62a68e525e8330134b8c4fadeba409c
Fredrik Jonsson <fjon7@kth.se>
2016-12-08 21:17:16 +0100

    Ny fil, testfil sms, ny klass testklass

---
+++ src/funktionalityTest.java
@@ -0,0 +1,13 @@
+
+public class funktionalityTest {
+
+	public funktionalityTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
